18364be3d38a38b19a8590e5f535387179bfa0f3
KgothatsoManganye <kpmanganye09@gmail.com>
2016-01-26 10:25:05 +0200

    App
    Database Dictionary

---
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void connection()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DS-SYS-LNR2-LT;Initial Catalog=DB_Dictionary;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        
+
+    }
+}
