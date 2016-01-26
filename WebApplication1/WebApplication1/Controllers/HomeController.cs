a00990f681660c72da66f4295e5ec196651efa5a
KgothatsoManganye <kpmanganye09@gmail.com>
2016-01-26 17:51:20 +0200

    Changes

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            ViewBag.MyName = "Whaever";
             return View();
         }
 
