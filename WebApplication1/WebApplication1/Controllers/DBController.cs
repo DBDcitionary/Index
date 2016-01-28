82ddb02bc68b1a397bf96bd1553ab3baa87a6d47
KgothatsoManganye <kpmanganye09@gmail.com>
2016-01-28 17:24:35 +0200

    New Changes

--- WebApplication1/WebApplication1/Controllers/DBController.cs
+++ WebApplication1/WebApplication1/Controllers/DBController.cs
@@ -9,9 +9,16 @@ namespace WebApplication1.Controllers
     public class DBController : Controller
     {
         DB_DictionaryContext db = new DB_DictionaryContext();
-        // GET: DB_Infor_
+        // GET: DB Information
         public ActionResult Index()
         {
+
+            return View();
+        }
+
+        public ActionResult DatabaseInformation()
+        {
+
             return View(db.Database_Tbl.ToList());
         }
     }
