fa7beae86dd97395f384cd991ae111fe9669b5f7
KgothatsoManganye <kpmanganye09@gmail.com>
2016-01-28 16:17:22 +0200

    Comit

---
+++ WebApplication1/WebApplication1/Controllers/DBController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DBController : Controller
+    {
+        DB_DictionaryContext db = new DB_DictionaryContext();
+        // GET: DB_Infor_
+        public ActionResult Index()
+        {
+            return View(db.Database_Tbl.ToList());
+        }
+    }
+}
