daeb7fcdcaf0afc6b2a3dde28211fe00888995aa
KgothatsoManganye <kpmanganye09@gmail.com>
2016-02-02 15:10:24 +0200

    Revert "git commit"
    This reverts commit b6095f874a66c8ded77f5d994c2d7725fc7a946c.

--- WebApplication1/WebApplication1/Controllers/DBController.cs
+++ WebApplication1/WebApplication1/Controllers/DBController.cs
@@ -19,18 +19,70 @@ namespace WebApplication1.Controllers
 
         public ActionResult DatabaseInformation()
         {
-
-            return View(db.Database_Tbl.ToList());
+            //***************************************
+            //Getting Database Information
+            //**************************************
+            try
+            {
+                var dbInfo = db.Database_Tbl.ToList();//Listing Database Information
+                return View(dbInfo);//passing the view page with the results
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
         }
 
-        public ActionResult TableInformation()
+        public ActionResult TableInformation(int ? dB_ID)
         {
-            return View(db.Table_Tbl.ToList());
+            //***************************************
+            //Getting Table Information
+            //***************************************
+            try
+            {
+                //Condition to check the ID of the Database is not Empty
+                if (dB_ID == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                List<Table_Tbl> tblinfo = db.Table_Tbl.ToList();//listing Table Information based on the selected Database ID
+                var t1 = tblinfo.Where(t => t.DB_ID == dB_ID);//Filtering the list based on the selected Database ID
+                if (tblinfo == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(t1);//passing the Table View with the filtered results
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
         }
 
-        public ActionResult FieldInformation()
+        public ActionResult FieldInformation(int? tbl_ID)
         {
-            return View(db.Field_Tbl.ToList());
+            //***************************************
+            //Getting Field Information
+            //***************************************
+            try
+            {
+                //Condition to check the ID of the table is not Empty
+                if (tbl_ID == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                List<Field_Tbl> fldinfo = db.Field_Tbl.ToList();//Listing fields 
+                var f1 = fldinfo.Where(l => l.TBL_ID == tbl_ID);//Filtering results based on the selected TBL ID
+                if (fldinfo == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(f1);
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
         }
     }
 }
